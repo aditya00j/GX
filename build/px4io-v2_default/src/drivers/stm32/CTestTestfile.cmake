eea449e5533fe52ec652ae647c43cfe54a021dec
bluetulip89 <rajan@bluetronics.co.in>
2017-11-09 08:17:01 +0530

    Revised Phase1 build

--- build/px4io-v2_default/src/drivers/stm32/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rablsa/Pictures/Firmware/src/drivers/stm32
-# Build directory: /home/rablsa/Pictures/Firmware/build/px4io-v2_default/src/drivers/stm32
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/px4io-v2_default/src/firmware/nuttx/.gdbinit
+++
@@ -1,7 +0,0 @@
-target extended /dev/ttyACM0
-monitor swdp_scan
-attach 1
-monitor vector_catch disable hard
-set mem inaccessible-by-default off
-set print pretty
-source /home/rablsa/Pictures/Firmware/Debug/PX4
